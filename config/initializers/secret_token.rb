6eea6a2b02b2185149cae3bdb9e527279718742e
Jan Andersson <jan.andersson@gmail.com>
2013-01-24 11:59:52 +0100

    Initial setup of Rails 4.0 beta

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitgirot::Application.config.secret_key_base = 'f238415c691419486e353c67e61f42059be15e75ff1f0b8c6e0fca7c791c2a7dca97a0114ec2e0e1325d71a97d2dd1be6f9cda76c333d7150c83452bda07419d'
